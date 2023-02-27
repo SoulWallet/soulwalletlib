d5ddb22bc7574f57a4705eb227c19351cf18c231
zhangshengjie <z.cejay@gmail.com>
2023-02-27 11:50:13 +0800

    fixed bunlder rpc timeout setting

--- src/utils/httpRequest.ts
+++ src/utils/httpRequest.ts
@@ -4,7 +4,7 @@
  * @Autor: z.cejay@gmail.com
  * @Date: 2023-02-11 12:45:04
  * @LastEditors: cejay
- * @LastEditTime: 2023-02-11 13:02:03
+ * @LastEditTime: 2023-02-27 11:27:45
  */
 
 export class HttpRequest {
@@ -24,9 +24,9 @@ export class HttpRequest {
         return null;
     }
     static async post(url: string, data: any, timeout: number = 1000 * 30): Promise<any> {
+        const controller = new AbortController();
+        const id = setTimeout(() => controller.abort(), timeout);
         try {
-            const controller = new AbortController();
-            const id = setTimeout(() => controller.abort(), timeout);
             const response = await fetch(url, {
                 method: "POST",
                 signal: controller.signal,
@@ -35,13 +35,14 @@ export class HttpRequest {
                 },
                 body: JSON.stringify(data),
             });
-            clearTimeout(id);
             if (response.ok) {
                 const json = await response.json();
                 return json;
             }
         } catch (error) {
             console.log(error);
+        } finally {
+            clearTimeout(id);
         }
         return null;
     }
