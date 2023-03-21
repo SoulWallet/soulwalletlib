cd715d3f5db3e462dce0ea9b5b89bdb04f58c9fd
zhangshengjie <z.cejay@gmail.com>
2023-03-21 11:12:38 +0800

    update fromTransaction

--- dist/utils/converter.js
+++ dist/utils/converter.js
@@ -5,7 +5,7 @@
  * @Autor: z.cejay@gmail.com
  * @Date: 2022-11-07 21:08:08
  * @LastEditors: cejay
- * @LastEditTime: 2023-03-10 19:21:43
+ * @LastEditTime: 2023-03-21 11:11:14
  */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Converter = void 0;
@@ -36,17 +36,25 @@ class Converter {
             throw new Error("transcation is empty");
         }
         let sender = (transcations[0].from || '').toLowerCase();
+        let callGasLimits = [];
         // #TODO if gas is null
         //let _callGasLimit: BigNumber = BigNumber.from(transcations[0].gas);
         const _to = [transcations[0].to];
         const _value = [ethers_1.BigNumber.from(transcations[0].value).toHexString()];
         const _data = [transcations[0].data];
+        if (transcations[0].gasLimit) {
+            callGasLimits.push(transcations[0].gasLimit);
+        }
         if (transcations.length > 1) {
             for (let i = 1; i < transcations.length; i++) {
                 // _callGasLimit.add(BigNumber.from(transcations[i]));
                 _to.push(transcations[i].to);
                 _value.push(ethers_1.BigNumber.from(transcations[i].value).toHexString());
                 _data.push(transcations[i].data);
+                const _gasLimit = transcations[i].gasLimit;
+                if (_gasLimit) {
+                    callGasLimits.push(_gasLimit);
+                }
                 const _sender = (transcations[i].from || '').toLowerCase();
                 if (sender !== _sender) {
                     throw new Error("transcation sender is not same");
@@ -63,7 +71,16 @@ class Converter {
             callData = new ethers_1.ethers.utils.Interface(ABI_1.execBatchFromEntryPoint)
                 .encodeFunctionData("execFromEntryPoint", [_to, _value, _data]);
         }
-        const op = new userOperation_1.UserOperation(sender, nonce, undefined, callData, undefined, maxFeePerGas, maxPriorityFeePerGas, paymasterAndData);
+        let callGasLimit = ethers_1.BigNumber.from(0);
+        if (callGasLimits.length === transcations.length) {
+            for (let i = 0; i < callGasLimits.length; i++) {
+                callGasLimit = callGasLimit.add(callGasLimits[i]);
+            }
+            if (callGasLimit.gt(0)) {
+                callGasLimit = callGasLimit.add(30000);
+            }
+        }
+        const op = new userOperation_1.UserOperation(sender, nonce, undefined, callData, callGasLimit.toHexString(), maxFeePerGas, maxPriorityFeePerGas, paymasterAndData);
         // let gasEstimated = await op.estimateGas(entryPointAddress,
         //     etherProvider
         // );
