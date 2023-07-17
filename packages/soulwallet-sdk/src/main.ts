4f901676f340133a3adec4b1bbdcc79ff9a2d3d0
Jayden <z.cejay@gmail.com>
2023-07-17 15:41:09 +0800

    update

--- packages/soulwallet-sdk/src/main.ts
+++ packages/soulwallet-sdk/src/main.ts
@@ -1,7 +1,12 @@
 import { SoulWallet } from "./soulWallet.js";
 import { L1KeyStore } from "./L1KeyStore.js";
+import { UserOperation } from "./interface/ISoulWallet.js";
+import { UserOpErrors, UserOpErrorCodes } from "./interface/IUserOpErrors.js";
 
 export {
+    UserOperation,
     SoulWallet,
+    UserOpErrors,
+    UserOpErrorCodes,
     L1KeyStore
 }
